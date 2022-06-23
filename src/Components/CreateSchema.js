56a05b3d357973bcb4557c38d158cbbdffe59e54
wenkuan Xiao <93316790+LazyDreamingDog@users.noreply.github.com>
2022-06-23 22:47:08 +0800

    Update CreateSchema.js

--- src/Components/CreateSchema.js
+++ src/Components/CreateSchema.js
@@ -15,7 +15,7 @@ import axios from "axios";
 
 const Contex = createContext(undefined);
 
-const ip_port = "http://192.168.80.130:8080/";
+const ip_port = "http://139.9.181.146:8080/";
 
 const { Step } = Steps;
 const { Panel } = Collapse;
