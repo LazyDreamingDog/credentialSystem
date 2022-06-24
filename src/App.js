b80d75b8b67b1d98b91cac5bcf3308ccb519084a
LazyDreamingDog <19271135@bjtu.edu.cn>
2022-06-25 02:28:47 +0800

    new

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.less';
+import PageRouter from "./Router/PageRouter";
+
+const App = () => (
+    <div className="App">
+        <PageRouter />
+    </div>
+);
+
+export default App;
