a99c6acbe549572122c0a62ef17c8733c247c9fb
wenkuan Xiao <93316790+LazyDreamingDog@users.noreply.github.com>
2022-06-25 19:16:52 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-    <App />,
+    <PageRouter />,
   document.getElementById('root')
 );
 
